1d9197858a263e89b6a1be428403fa72f221de15
wfliu0327 <liuweifeng0327@gmail.com>
2025-03-10 08:36:53 +0800

    test

--- team_code.py
+++ team_code.py
@@ -32,6 +32,70 @@ from sklearn.model_selection import train_test_split
 
 # Train your models. This function is *required*. You should edit this function to add your code, but do *not* change the arguments
 # of this function. If you do not train one of the models, then you can return None for the model.
+class focal_loss(nn.Module):
+    def __init__(self, alpha=0.25, gamma=2, num_classes = 2, size_average=True):
+        """
+        focal_loss损失函数, -α(1-yi)**γ *ce_loss(xi,yi)
+        步骤详细的实现了 focal_loss损失函数.
+        :param alpha:   阿尔法α,类别权重. 当α是列表时,为各类别权重,当α为常数时,类别权重为[α, 1-α, 1-α, ....],常用于 目标检测算法中抑制背景类 , retainnet中设置为0.255
+        :param gamma:   伽马γ,难易样本调节参数. retainnet中设置为2
+        :param num_classes:     类别数量
+        :param size_average:    损失计算方式,默认取均值
+        """
+        super(focal_loss,self).__init__()
+        self.size_average = size_average
+        if isinstance(alpha,list):
+            assert len(alpha)==num_classes   # α可以以list方式输入,size:[num_classes] 用于对不同类别精细地赋予权重
+            print(" --- Focal_loss alpha = {}, 将对每一类权重进行精细化赋值 --- ".format(alpha))
+            self.alpha = torch.Tensor(alpha)
+        else:
+            assert alpha<1   #如果α为一个常数,则降低第一类的影响,在目标检测中为第一类
+            print(" --- Focal_loss alpha = {} ,将对背景类进行衰减,请在目标检测任务中使用 --- ".format(alpha))
+            self.alpha = torch.zeros(num_classes)
+            self.alpha[0] += alpha
+            self.alpha[1:] += (1-alpha) # α 最终为 [ α, 1-α, 1-α, 1-α, 1-α, ...] size:[num_classes]
+
+        self.gamma = gamma
+
+    def forward(self, preds, labels):
+        """
+        focal_loss损失计算
+        :param preds:   预测类别. size:[B,N,C] or [B,C]    分别对应与检测与分类任务, B批次, N检测框数, C类别数
+        :param labels:  实际类别. size:[B,N] or [B]        [B*N个标签(假设框中有目标)]，[B个标签]
+        :return:
+        """
+                
+        #固定类别维度，其余合并(总检测框数或总批次数)，preds.size(-1)是最后一个维度
+        preds = preds.view(-1,preds.size(-1))
+        self.alpha = self.alpha.to(preds.device)
+        
+        #使用log_softmax解决溢出问题，方便交叉熵计算而不用考虑值域
+        preds_logsoft = F.log_softmax(preds, dim=1) 
+        
+     	#log_softmax是softmax+log运算，那再exp就算回去了变成softmax
+        preds_softmax = torch.exp(preds_logsoft)    
+   
+        # 这部分实现nll_loss ( crossentropy = log_softmax + nll)
+        preds_softmax = preds_softmax.gather(1,labels.view(-1,1)) 
+        preds_logsoft = preds_logsoft.gather(1,labels.view(-1,1))
+
+        self.alpha = self.alpha.gather(0,labels.view(-1))
+
+        # torch.pow((1-preds_softmax), self.gamma) 为focal loss中 (1-pt)**γ
+        
+        #torch.mul 矩阵对应位置相乘，大小一致
+        loss = -torch.mul(torch.pow((1-preds_softmax), self.gamma), preds_logsoft) 
+    
+        #torch.t()求转置
+        loss = torch.mul(self.alpha, loss.t())
+        #print(loss.size()) [1,5]
+        
+        if self.size_average:
+            loss = loss.mean()
+        else:
+            loss = loss.sum()
+       
+        return loss
 
 # Train your model.
 def train_model(data_folder, model_folder, verbose):
@@ -76,20 +140,21 @@ def train_model(data_folder, model_folder, verbose):
         
     model = ResNet1D(
         in_channels=12, 
-        base_filters=128, # 64 for ResNet1D, 352 for ResNeXt1D
+        base_filters=64, # 64 for ResNet1D, 352 for ResNeXt1D
         kernel_size=16, 
         stride=2, 
         groups=32, 
         n_block=48, 
-        n_classes=2, 
+        n_classes=1, 
         downsample_gap=6, 
         increasefilter_gap=12, 
         use_do=True)
     model.to(device)  
 
     # Create Loss and Optimizer
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-3)
+    # criterion = nn.CrossEntropyLoss()
+    criterion = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([1.5]).to(device))
+    optimizer = optim.Adam(model.parameters(), lr=1e-2, weight_decay=1e-4)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=10)
 
 
@@ -104,8 +169,10 @@ def train_model(data_folder, model_folder, verbose):
     best_score = 0
     epoch = 0
     from tqdm import tqdm
-    for _ in tqdm(range(10), desc="epoch", leave=False):
+    for _ in tqdm(range(100), desc="epoch", leave=False):
         model.train()
+        all_pred_prob = []
+        all_gt = []
         prog_iter = tqdm(train_loader, desc="Training", leave=False)
         for i, (signals, labels) in enumerate(prog_iter):
                 signals, labels = signals.to(device), labels.to(device)
@@ -113,9 +180,13 @@ def train_model(data_folder, model_folder, verbose):
                 # if labels.sum() == 0:
                 #     # print(f"Skipping batch {i}, no positive samples.")
                 #     continue
-                outputs = model(signals)
+                # print(labels)
+                # if i > 5:
+                #     break
+                outputs = model(signals).squeeze()
                 # print(labels.shape, outputs.shape)
-                loss1 = criterion(outputs, labels)
+                # print(f"signals device: {signals.device}, labels device: {labels.device}, outputs device: {outputs.device}")
+                loss1 = criterion(outputs.float(), labels.float())
                 # loss2 = auc_loss(outputs, labels)
                 # print(loss2)
                 loss = loss1 
@@ -123,11 +194,33 @@ def train_model(data_folder, model_folder, verbose):
                 loss.backward()
                 optimizer.step()
                 step += 1
+                prob_class_1 = torch.sigmoid(outputs).detach().cpu().numpy() 
+                all_pred_prob.append(prob_class_1)
+                print()
+                print("y_true:",labels.cpu().numpy())
+                print("y_pred:",(prob_class_1 > 0.5).astype(int))
+                all_gt.append(labels.cpu().numpy())
                 writer.add_scalar('Loss/train_CELOSS', loss1.item(), step)
                 # writer.add_scalar('Loss/train_Aucloss', loss2, step)
                 # writer.add_scalar('Loss/train', loss, step)
                 # print(f"Epoch [{epoch+1}/300], Iteration {i}, Loss: {loss.item():.4f}")
-            
+        all_pred_prob = np.concatenate(all_pred_prob)
+        all_pred = (all_pred_prob > 0.5).astype(int)
+        all_gt = np.concatenate(all_gt)
+        
+        challenge_score = compute_challenge_score(all_gt, all_pred_prob)
+        auroc, auprc = compute_auc(all_gt, all_pred_prob)
+        accuracy = compute_accuracy(all_gt, all_pred)
+        f_measure = compute_f_measure(all_gt, all_pred)
+
+        writer.add_scalar('train/challenge_score', challenge_score, _)
+        writer.add_scalar('train/auroc', auroc, _)
+        writer.add_scalar('train/auprc', auprc, _)
+        writer.add_scalar('train/accuracy', accuracy, _)
+        writer.add_scalar('train/f_measure', f_measure, _)
+
+
+
         scheduler.step(_) 
         model.eval()
         all_pred_prob = []
@@ -135,10 +228,14 @@ def train_model(data_folder, model_folder, verbose):
         with torch.no_grad():
             prog_iter = tqdm(test_loader, desc="Testing", leave=False)
             for i, (signals, labels) in enumerate(prog_iter):
+                    
                     signals, labels = signals.to(device), labels.to(device)
-                    outputs = model(signals)
-                    prob_class_1 = torch.sigmoid(outputs)[:, 1].cpu().numpy() 
+                    outputs = model(signals).squeeze()
+                    prob_class_1 = torch.sigmoid(outputs).cpu().numpy() 
                     all_pred_prob.append(prob_class_1)
+                    print()
+                    print("y_true:", labels.cpu().numpy())
+                    print("y_pred:", (prob_class_1 > 0.5).astype(int))
                     all_gt.append(labels.cpu().numpy()) 
         all_pred_prob = np.concatenate(all_pred_prob)
         all_pred = (all_pred_prob > 0.5).astype(int)
@@ -149,11 +246,11 @@ def train_model(data_folder, model_folder, verbose):
         accuracy = compute_accuracy(all_gt, all_pred)
         f_measure = compute_f_measure(all_gt, all_pred)
 
-        writer.add_scalar('challenge_score', challenge_score, _)
-        writer.add_scalar('auroc', auroc, _)
-        writer.add_scalar('auprc', auprc, _)
-        writer.add_scalar('accuracy', accuracy, _)
-        writer.add_scalar('f_measure', f_measure, _)
+        writer.add_scalar('val/challenge_score', challenge_score, _)
+        writer.add_scalar('val/auroc', auroc, _)
+        writer.add_scalar('val/auprc', auprc, _)
+        writer.add_scalar('val/accuracy', accuracy, _)
+        writer.add_scalar('val/f_measure', f_measure, _)
         # 保存最佳模型
         if challenge_score > best_score:
             save_model(model_folder, model)
@@ -171,12 +268,12 @@ def load_model(model_folder, verbose):
     # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = ResNet1D(
         in_channels=12, 
-        base_filters=128, # 64 for ResNet1D, 352 for ResNeXt1D
+        base_filters=64, # 64 for ResNet1D, 352 for ResNeXt1D
         kernel_size=16, 
         stride=2, 
         groups=32, 
         n_block=48, 
-        n_classes=2, 
+        n_classes=1, 
         downsample_gap=6, 
         increasefilter_gap=12, 
         use_do=True)
@@ -202,7 +299,7 @@ def run_model(record, model, verbose):
     outputs = model(signal)
     # Get the model outputs.
     
-    probability_output = torch.sigmoid(outputs)[:, 1].cpu().detach().numpy() 
+    probability_output = torch.sigmoid(outputs).cpu().detach().numpy() 
     if probability_output > 0.5:
         binary_output = 1 
     else:
